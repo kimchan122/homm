ac0b3e90611d061ecd7bbb9672846312413a5e2e
kimchan122 <chanchankim122@gmail.com>
2023-08-16 00:42:28 +0900

    infura api key setting

--- app/next.config.js
+++ app/next.config.js
@@ -2,3 +2,14 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+/* dotenv */
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
+
+module.exports = {
+    webpack(config) {
+        config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+        return config;
+    },
+};
