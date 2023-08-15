4fe06f92c94137d9220b9c945ceaf2fa1180ae9b
kimchan122 <chanchankim122@gmail.com>
2023-08-15 23:31:59 +0900

    init

---
+++ app/pages/ens.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ENSPage() {
+  return (
+    <div>
+      <h1>ENS Practice</h1>
+    </div>
+  );
+}
+
+export default ENSPage;
---
+++ app/pages/spruceid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SpruceIDPage() {
+  return (
+    <div>
+      <h1>SpruceID Practice</h1>
+    </div>
+  );
+}
+
+export default SpruceIDPage;
