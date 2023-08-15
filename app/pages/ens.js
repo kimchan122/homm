cf8a528570dfd3a00f3b368fcbfe60ffe2a2f319
kimchan122 <chanchankim122@gmail.com>
2023-08-15 23:40:24 +0900

    added dotenv

--- app/pages/ens.js
+++ app/pages/ens.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 function ENSPage() {
-  return (
-    <div>
-      <h1>ENS Practice</h1>
-    </div>
-  );
+    const router = useRouter();
+    const { ensName } = router.query;
+
+    return (
+        <div>
+            <h1>ENS Practice</h1>
+            <h3>ENS name: {ensName}</h3>
+        </div>
+    );
 }
 
 export default ENSPage;
