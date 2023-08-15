6feb60bcf59aefe7cffb4497318528c615a0d1fd
kimchan122 <chanchankim122@gmail.com>
2023-08-16 01:21:48 +0900

    my ethereum address

--- app/pages/ens.js
+++ app/pages/ens.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
 
 function ENSPage() {
-    const router = useRouter();
-    const { ensName } = router.query;
+    const [ethereumAddress, setEthereumAddress] = useState('');
+
+    useEffect(() => {
+        async function fetchEthereumAddress() {
+            if (window.ethereum) {
+                try {
+                    await window.ethereum.enable();
+                    const web3 = new Web3(window.ethereum);
+                    const accounts = await web3.eth.getAccounts();
+
+                    if (accounts.length > 0) {
+                        setEthereumAddress(accounts[0]);
+                    } else {
+                        setEthereumAddress('Not connected');
+                    }
+                } catch (error) {
+                    console.error(error);
+                    setEthereumAddress('Error connecting');
+                }
+            } else {
+                setEthereumAddress('No wallet detected');
+            }
+        }
+
+        fetchEthereumAddress();
+    }, []);
 
     return (
         <div>
             <h1>ENS Practice</h1>
-            <h3>ENS name: {ensName}</h3>
+            <p>Ethereum Address: {ethereumAddress}</p>
         </div>
     );
 }
