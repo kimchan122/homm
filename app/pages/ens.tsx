525d77cb27a2a1a20af331d1fea02c8be14c4d43
kimchan122 <chanchankim122@gmail.com>
2023-08-16 18:02:35 +0900

    typescript

---
+++ app/pages/ens.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+
+const ENSPage = () => {
+
+    const [ethereumAddress, setEthereumAddress] = useState('');
+
+    return (
+        <div>
+            <h1>ENS practice</h1>
+        </div >
+    );
+}
+
+export default ENSPage;
---
+++ app/pages/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>HOMM</h1>
+            <Link href="/ens">ENS Practice</Link>
+        </div >
+    );
+}
+
+export default HomePage;
